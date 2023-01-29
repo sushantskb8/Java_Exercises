996dc312274981241caf41ec649e8dccd941987e
sushantskb <>
2023-01-30 01:14:52 +0530

    Test1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = input.nextInt();
+        System.out.println(num);
     }
 }
